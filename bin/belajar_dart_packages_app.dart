73c4e3fad949105227d06e52f7e7bd8890b74d5e
Ahmad Hazza <ikhwan.hazza7@gmail.com>
2022-04-17 23:06:24 +0700

    first commit

---
+++ bin/belajar_dart_packages_app.dart
@@ -0,0 +1,9 @@
+import 'package:dart_packages_library_pzn/hello.dart' as belajar;
+
+void main() {
+  print(belajar.sayHi("Jaka"));
+
+  var customer = belajar.Customer();
+  var category = belajar.Category();
+  var product = belajar.Product();
+}
---
+++ test/belajar_dart_packages_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_packages_app/belajar_dart_packages_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
